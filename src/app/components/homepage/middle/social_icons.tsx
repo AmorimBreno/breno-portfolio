f8716860ebf2fcace3515268401f7273bc92d5d8
Breno Amorim <brenoamorim11@gmail.com>
2025-03-21 09:50:26 -0300

    icon animation and links to socials

--- src/app/components/homepage/middle/social_icons.tsx
+++ src/app/components/homepage/middle/social_icons.tsx
@@ -3,8 +3,20 @@ import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 export function SocialIcons() {
   return (
     <div className="mt-8 flex h-20 w-full items-center justify-center gap-12">
-      <FaLinkedinIn className="size-14 rounded-lg bg-purple-900 p-2 text-white hover:size-16"></FaLinkedinIn>
-      <FaGithub className="size-14 rounded-lg text-purple-900 hover:size-16"></FaGithub>
+      <a
+        href="https://www.linkedin.com/in/brenoamorimroman/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedinIn className="size-14 rounded-lg border-purple-900 bg-purple-900 p-2 text-white transition-all duration-100 ease-in-out hover:border-2 hover:bg-white hover:text-purple-900"></FaLinkedinIn>
+      </a>
+      <a
+        href="https://github.com/AmorimBreno"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub className="size-14 rounded-lg text-purple-900 transition-all duration-100 ease-in-out hover:rounded-full hover:border-2 hover:bg-purple-900 hover:text-white"></FaGithub>
+      </a>
     </div>
   )
 }
